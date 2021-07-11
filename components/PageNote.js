18aa019931b025ee27c1be2c358f02afa4b0cee8
Brian Saunders <saundersbd@gmail.com>
2021-07-11 15:19:12 -0500

    Add image zoom

--- components/PageNote.js
+++ components/PageNote.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const PageNote = ({ title, description }) => (
   <div className="bg-blue-100 px-6 py-4 rounded-md dark:bg-gray-900">
-    {title && <h3 className="text-lg mb-2 mt-0 dark:text-gray-400">{title}</h3>}
-    <p className="mb-0 mt-0 text-base leading-loose">{description}</p>
+    {title && <h3 className="text-lg mb-1 mt-0 dark:text-gray-400">{title}</h3>}
+    <p className="mb-0 mt-0 text-lg leading-loose">{description}</p>
   </div>
 );
 
--- components/PostImage.js
+++ components/PostImage.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Image from "next/image";
+import Zoom from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
 
 const PostImage = ({ alt, className, caption, src, width, height }) => {
   const img = (
@@ -10,7 +12,7 @@ const PostImage = ({ alt, className, caption, src, width, height }) => {
 
   return (
     <figure className="mx-0 md:-mx-8">
-      {img}
+      <Zoom>{img}</Zoom>
       {caption && (
         <figcaption className="mx-0 md:mx-8 mt-1 text-base text-gray-500 dark:text-gray-300 leading-loose">
           {caption}
