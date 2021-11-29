eae8d715e086c577d62687f2d621ac586625141d
Brian Saunders <saundersbd@gmail.com>
2021-11-29 05:32:07 -0600

    Upgrade dependencies

--- components/Button.js
+++ components/Button.js
@@ -10,9 +10,10 @@ const Button = ({
   ...props
 }) => {
   const btnClass = cn({
-    "flex items-center justify-center text-base font-semibold tracking-wide rounded-md no-underline hover:no-underline focus:outline-none focus:ring focus:ring-orange py-2 px-3 text-white bg-navy hover:bg-teal border border-navy hover:border-teal dark:border-navy-light dark:bg-navy-dark dark:text-white  dark:hover:bg-navy dark:hover:text-white":
+    "flex items-center justify-center text-base font-semibold tracking-wide rounded-md no-underline hover:no-underline focus:outline-none focus:ring focus:ring-orange py-2 px-3": true,
+    "text-white bg-navy hover:bg-teal border border-navy hover:border-teal dark:border-navy-light dark:bg-navy-dark dark:text-white  dark:hover:bg-navy dark:hover:text-white":
       primary,
-    "flex items-center justify-center text-base font-semibold tracking-wide rounded-md no-underline hover:no-underline focus:outline-none focus:ring focus:ring-orange py-2 px-3 text-navy border border-navy bg-navy-lightest hover:bg-navy hover:text-white dark:border-navy-light dark:bg-navy-dark dark:text-white dark:hover:bg-navy dark:hover:text-white":
+    "text-navy border border-navy bg-navy-lightest hover:bg-navy hover:text-white dark:border-navy-light dark:bg-navy-dark dark:text-white dark:hover:bg-navy dark:hover:text-white":
       secondary,
   });
   const isInternalLink = href && (href.startsWith("/") || href.startsWith("#"));
