2626766fb799686162273882e48123a0adeff940
Brian Saunders <saundersbd@gmail.com>
2022-08-08 08:47:11 -0500

    New post

--- components/PostListItem.js
+++ components/PostListItem.js
@@ -6,7 +6,9 @@ const Post = ({ slug, title, description }) => {
     <li className="mb-6">
       <Link href={`/posts/${slug.replace(/\.mdx?$/, "")}`} passHref>
         <a className="flex mb-1 sm:w-max text-black dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 rounded -mx-1 -my-0.5 px-1 py-0.5 underline focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-50 dark:focus:bg-gray-900">
-          <h3 className="text-sm font-bold tracking-wide">{title}</h3>
+          <h3 className="text-base font-semibold tracking-wide font-display">
+            {title}
+          </h3>
         </a>
       </Link>
 
