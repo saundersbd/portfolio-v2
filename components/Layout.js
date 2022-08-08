d5acd611fddcc4cc1170d733bbf003a9279825ee
Brian Saunders <saundersbd@gmail.com>
2022-08-08 08:14:09 -0500

    Overflow hidden fix on Safari

--- components/Layout.js
+++ components/Layout.js
@@ -6,7 +6,7 @@ import Footer from "../components/Footer";
 const Layout = ({ className, children }) => (
   <div>
     <Ribbon />
-    <div className="overflow-hidden shadow-lg bg-gradient-to-b from-white to-gray-50 dark:from-gray-900 dark:to-gray-900 rounded-2xl">
+    <div className="relative z-0 overflow-hidden shadow-lg bg-gradient-to-b from-white to-gray-50 dark:from-gray-900 dark:to-gray-900 rounded-2xl">
       <Header />
       <div className={`max-w-2xl mx-auto px-6 sm:px-4 ${className}`}>
         {children}
