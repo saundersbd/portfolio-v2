50a99fe5c4d73518d578f1ca1e014080d51324a1
Brian Saunders <saundersbd@gmail.com>
2022-10-28 16:12:31 -0500

    New post

--- components/Ribbon.js
+++ components/Ribbon.js
@@ -5,10 +5,10 @@ const Ribbon = () => (
   <div className="flex items-center justify-center w-full py-3 text-sm leading-5 text-center text-white bg-gray-900 dark:bg-gray-800 dark:text-gray-200">
     Featured post:{" "}
     <Link
-      href="/posts/prescriptive-feedback"
+      href="/posts/imposter-syndrome"
       className="px-1 mr-1 font-medium underline transition-all rounded text-mint-300 focus:outline-none focus:ring focus:ring-mint-400 hover:text-mint-200"
     >
-      Prescriptive Feedback
+      My Good Friend Imposter Syndrome
     </Link>{" "}
     →
   </div>
