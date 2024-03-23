2b5be52e0ae6e1505b566be750bb7bf80a8fdb5a
Brian Saunders <saundersbd@gmail.com>
2024-03-23 11:40:44 -0500

    Update airtable integration

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => (
   <div className="flex items-center justify-center py-4 font-sans text-xs text-center border-t text-slate-11 border-slate-6 dark:border-gray-800 sm:text-sm sm:py-6 dark:text-gray-400">
-    Designed and built by Brian Saunders &middot; &copy;2023
+    Designed and built by Brian Saunders &middot; &copy;2024
   </div>
 );
 
